63b177136840f304d8f06b259f1baf0d7ca17ed2
AndrewVoloshin <voloshinandy@gmai.com>
2024-09-10 20:18:06 +0300

    feat: template order item and add scss

---
+++ src/components/OrderItem.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const people = ref([
+    { name: 'John Doe', age: 28, city: 'New York' },
+    { name: 'Jane Smith', age: 34, city: 'Los Angeles' },
+    { name: 'Bob Johnson', age: 23, city: 'Chicago' },
+    { name: 'Alice Brown', age: 41, city: 'Houston' },
+]);
+</script>
+<template>
+
+
+    <div class=" w-10 h-10 ">
+        <table class="table">
+            <tbody>
+                <tr v-for="(person, index) in people"
+                    :key="index">
+                    <td>{{ index + 1 }}</td>
+                    <td>{{ person.name }}</td>
+                    <td>{{ person.age }}</td>
+                    <td>{{ person.city }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.table {
+    width: 100%;
+    border-collapse: separate;
+    border-spacing: 0 10px;
+
+    tr {
+        background: white;
+    }
+}
+
+.table td {
+    border: 1px solid #ddd;
+    padding: 8px;
+    text-align: left;
+    border-left: none;
+    border-right: none;
+
+    &:first-child {
+        border-left: 1px solid #ddd;
+        border-top-left-radius: 5px;
+        border-bottom-left-radius: 5px;
+    }
+
+    &:last-child {
+        border-right: 1px solid #ddd;
+        border-top-right-radius: 5px;
+        border-bottom-right-radius: 5px;
+    }
+}
+</style>
--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -1,9 +1,18 @@
+<script setup lang="ts">
+import OrderItem from '../components/OrderItem.vue'
+
+</script>
+
 <template>
-  <div class="card w-25 h-25" >
-    <h1>This is an about page</h1>
+  <div class="order-container">
+    <h2>Orders</h2>
+    <OrderItem />
   </div>
 </template>
 
-<style>
 
+<style scoped>
+.order-container {
+  padding: 50px 0 0 40px;
+}
 </style>
